a417013834c5464c8a06eace748b25c88a032d91
Esraa Fathy <fesraa240@gmail.com>
2021-01-29 19:51:30 +0200

    Dao and entity of room

---
+++ app/src/main/java/com/example/tripreminder/RoomDataBase/TripDAO.java
@@ -0,0 +1,29 @@
+package com.example.tripreminder.RoomDataBase;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TripDAO {
+
+    @Insert
+    void Insert(TripTable tripTable);
+
+    @Update
+    void Update(TripTable tripTable);
+
+    @Delete
+    void Delete(TripTable tripTable);
+
+    @Query("DELETE FROM trips")
+    void deleteAllRecords();
+
+    @Query("SELECT * FROM trips")
+    LiveData<List<TripTable>> getAllRecords();
+}
---
+++ app/src/main/java/com/example/tripreminder/RoomDataBase/TripTable.java
@@ -0,0 +1,75 @@
+package com.example.tripreminder.RoomDataBase;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "trips")
+public class TripTable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(defaultValue = "Trip Tile",name = "title")
+    private String title;
+    private String time;
+    private String date;
+    private boolean status;
+    private String repetition;
+    private boolean ways;
+    private String from;
+    private String to;
+    private String notes;
+
+    public TripTable(String title, String time, String date, boolean status, String repetition, boolean ways, String from, String to, String notes) {
+        this.title = title;
+        this.time = time;
+        this.date = date;
+        this.status = status;
+        this.repetition = repetition;
+        this.ways = ways;
+        this.from = from;
+        this.to = to;
+        this.notes = notes;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public String getRepetition() {
+        return repetition;
+    }
+
+    public boolean getWays() {
+        return ways;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
