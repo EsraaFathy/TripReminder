1dc57d86d77cf18e3d3143bd9694361e711df3fb
Esraa Fathy <fesraa240@gmail.com>
2021-01-30 16:13:39 +0200

    bottom Navigation View

--- app/src/main/java/com/example/tripreminder/BaseHomeActivity.java
+++ app/src/main/java/com/example/tripreminder/BaseHomeActivity.java
@@ -1,21 +1,34 @@
 package com.example.tripreminder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.room.util.DBUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.tripreminder.Fragments.HistoryFragment;
+import com.example.tripreminder.Fragments.HomeFragment;
+import com.example.tripreminder.Fragments.ProfileFragment;
 import com.example.tripreminder.databinding.ActivityBaseHomeBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BaseHomeActivity extends AppCompatActivity {
-ActivityBaseHomeBinding activityBaseHomeBinding;
+    ActivityBaseHomeBinding activityBaseHomeBinding;
+    Fragment baseFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityBaseHomeBinding= DataBindingUtil.setContentView(this,R.layout.activity_base_home);
+        activityBaseHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_base_home);
+        baseFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentBasic, baseFragment).commit();
+
         activityBaseHomeBinding.addTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +36,34 @@ ActivityBaseHomeBinding activityBaseHomeBinding;
                 startActivity(new Intent());
             }
         });
+
+        activityBaseHomeBinding.bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
     }
+
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+
+            switch (menuItem.getItemId()) {
+                case R.id.homeItem_menu:
+                    baseFragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentBasic, baseFragment).commit();
+                    break;
+
+                case R.id.profileItem_menu:
+                    baseFragment = new ProfileFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentBasic, baseFragment).commit();
+                    break;
+
+                case R.id.historyItem_menu:
+                    baseFragment = new HistoryFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentBasic, baseFragment).commit();
+                    break;
+
+            }
+            return true;
+        }
+    };
 }
